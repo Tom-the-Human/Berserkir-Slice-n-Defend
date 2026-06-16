8b1c8735c762108e962ec2d87f92969742f9fbb9
Tom-the-Human <tomhodges83@gmail.com>
2026-06-16 12:22:21 -0400

    enemy hit zone outline added

--- Enemy-sys/enemy.gd
+++ Enemy-sys/enemy.gd
@@ -11,6 +11,10 @@ var end_pos : Vector2 # Forground/hit zone
 
 @onready var polygon_node: Polygon2D = $Polygon2D
 
+# hit zone entered outline
+var outline_line: Line2D
+var in_hit_zone := false
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	var center_x := 540.0
@@ -31,6 +35,15 @@ func _ready() -> void:
 	
 	position = start_pos
 	scale = Vector2(0.1, 0.1)
+	
+	# outline setup
+	outline_line = Line2D.new()
+	outline_line.points = polygon_node.polygon
+	outline_line.closed = true # Automatically connects the last point to the first
+	outline_line.width = 0.0   # Keep it invisible until it reaches the Hit Zone
+	outline_line.default_color = Color(1.5, 0.2, 0.2) # Over-bright red for a subtle glow effect
+	# Add it as a child of the polygon so it perfectly overlaps
+	polygon_node.add_child(outline_line)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -40,6 +53,13 @@ func _process(delta: float) -> void:
 	var perspective_curve := pow(progress, 3)
 	var scale_factor : Variant = lerp(0.1, 10.0, perspective_curve)
 	scale = Vector2(scale_factor, scale_factor)
+	
+	if progress >= 0.75 and not in_hit_zone:
+		in_hit_zone = true
+		# debug
+		print("hit zone entered")
+		# change outline_width to make visible
+		outline_line.width = 2.5
 	
 	if progress >= 1.0:
 		hit_player()
@@ -73,6 +93,7 @@ func create_falling_rigidbody(points: PackedVector2Array) -> void:
 	var col := CollisionPolygon2D.new()
 	
 	poly.polygon = scaled_points
+	# try to implement texture later (UV maps)
 	poly.color = Color.DARK_RED
 	col.polygon = scaled_points
 
